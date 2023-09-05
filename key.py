6a28795ee5017e81a02a2d8820c6928766c234da
GodyrXDD <63668889+GodyrXDD@users.noreply.github.com>
2023-09-05 14:16:46 -0700

    Update key.py

--- key.py
+++ key.py
@@ -1,4 +1,5 @@
-API_KEY = 'RGAPI-0094e7ad-5e60-4d7d-a91c-4274c0d489fb'
+# get key from https://developer.riotgames.com/
+API_KEY = ''
 # from api_key_holder import API_KEY
 # Use the API key in your program
 # print("API key:", API_KEY)
